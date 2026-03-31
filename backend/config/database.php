f24c8c974e42ce41cc975f61f00ee5b1372aacfa
IoneMorera <40915228+IoneMorera@users.noreply.github.com>
2026-04-01 01:32:57 +0200

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,24 +1,22 @@
 <?php
 
 return [
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
         'central' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ludoteca'),
-            'username' => env('DB_USERNAME', 'root'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'mysql' => [
