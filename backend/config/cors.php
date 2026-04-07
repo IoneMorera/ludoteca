a3421db60c3f8e06447508a3128cd1b22a306195
IoneMorera <40915228+IoneMorera@users.noreply.github.com>
2026-04-08 01:15:30 +0200

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'https://ludotecaraxar.netlify.app', 'http://localhost:5173')],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://.*netlify\.app'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
