323a78a532e9f95649221a73a5ec43339fbd2fbe
Ione Morera <ionemorera@gmail.com>
2026-04-21 00:19:34 +0200

    boostrap eliminar stateful

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         
         $middleware->api(
             prepend: [
-                \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
                 \Illuminate\Http\Middleware\HandleCors::class,
             ],
             remove: [
